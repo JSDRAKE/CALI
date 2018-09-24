fd435054efa9b6fd31fe4c0d7159438a360b4450
JSDRAKE <jsdrake@jsdrake.com.ar>
2018-09-24 14:28:09 -0300

    Agregado de Menu Info + Actualiza

--- app/src/main/java/ar/com/jsdrake/cali/Horarios.java
+++ app/src/main/java/ar/com/jsdrake/cali/Horarios.java
@@ -1,8 +1,11 @@
 package ar.com.jsdrake.cali;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.github.chrisbanes.photoview.PhotoView;
 
@@ -16,11 +19,26 @@ public class Horarios extends AppCompatActivity {
         PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
         photoView.setImageResource(R.drawable.horario);
     }
+    public void Informacion(View view){
+        Intent Informacion = new Intent(this, Informacion.class);
+        startActivity(Informacion);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
+    //@Override
+    public boolean onOptionsItemSelected(MenuItem opcion_menu) {
+        int id = opcion_menu.getItemId();
 
+        if (id == R.id.info) {
+            Informacion(null);
+            return true;
+        }
+        return super.onOptionsItemSelected(opcion_menu);
+
+    }
 }
--- app/src/main/java/ar/com/jsdrake/cali/Logica.java
+++ app/src/main/java/ar/com/jsdrake/cali/Logica.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class Logica extends AppCompatActivity {
@@ -17,11 +18,26 @@ public class Logica extends AppCompatActivity {
         Intent Regresar = new Intent(this, Materias.class);
         startActivity(Regresar);
     }
+    public void Informacion(View view){
+        Intent Informacion = new Intent(this, Informacion.class);
+        startActivity(Informacion);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
+    //@Override
+    public boolean onOptionsItemSelected(MenuItem opcion_menu) {
+        int id = opcion_menu.getItemId();
 
+        if (id == R.id.info) {
+            Informacion(null);
+            return true;
+        }
+        return super.onOptionsItemSelected(opcion_menu);
+
+    }
 }
