f3c138db1d2cd479f66df2cbc706262825512185
JSDRAKE <jsdrake@jsdrake.com.ar>
2018-09-25 07:08:49 -0300

    Agregado del Actualizar mediante Firebase

--- app/src/main/java/ar/com/jsdrake/cali/Materias.java
+++ app/src/main/java/ar/com/jsdrake/cali/Materias.java
@@ -1,19 +1,129 @@
 package ar.com.jsdrake.cali;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import android.widget.TextView;
 
 public class Materias extends AppCompatActivity {
+
+    String version_actual="1.0.0";
+    String version_firebase;
+    String url_firebase;
+
+    TextView txtVersion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materias);
+        txtVersion=(TextView) findViewById(R.id.txt_version);
+
+        txtVersion.setText("Version "+version_actual);
+        Obtener_Firebase();
+
+
     }
+
+    /*///////// Update /////////*/
+
+    private  void Obtener_Firebase(){
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+        DatabaseReference referencia_version,referencia_url;
+
+
+
+        referencia_url=database.getReference("url");
+        referencia_url.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                url_firebase=dataSnapshot.getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(Materias.this,"URL "+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        referencia_version=database.getReference("version");
+        referencia_version.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                version_firebase=dataSnapshot.getValue().toString();
+
+                if(version_firebase.trim().equals(version_actual.trim())){
+                    Toast.makeText(Materias.this,"No es Necesario Actualizar ", Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+
+                    AlertDialog.Builder ventana = new AlertDialog.Builder(Materias.this);
+                    ventana.setMessage("Existe una nueva version de la Aplicación");
+                    ventana.setTitle("CALI");
+                    ventana.setPositiveButton("Actualizar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent Actualizar = new Intent(Materias.this, PantallaActualizar.class);
+                            Actualizar.putExtra("version",version_firebase);
+                            //Actualizar.putExtra("url",url_firebase);
+                            finish();
+                            startActivity(Actualizar);
+                        }
+                    });
+
+                    ventana.setNegativeButton("Mas tarde", new DialogInterface.OnClickListener(){
+
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    ///No hace nada y se queda en la misma ventana
+                                }
+                            });
+
+                    ventana.show();
+
+
+/*
+                    Intent pantaActualizar=new Intent(getApplicationContext(),PantallaActualizar.class);
+                    pantaActualizar.putExtra("version",version_firebase);
+                    pantaActualizar.putExtra("url",url_firebase);
+                    finish();
+                    startActivity(pantaActualizar);*/
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(Materias.this,"Version "+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
+
+
+    }
+
+
+
+    /*////////// Ingreso a Materias //////////*/
 
 
     public void Regresar(View view) {
@@ -50,6 +160,15 @@ public class Materias extends AppCompatActivity {
 
     }
 
+    public void Actualizar(View view){
+        Intent Actualizar = new Intent(this, PantallaActualizar.class);
+        startActivity(Actualizar);
+
+    }
+
+
+    /*//////////// Menu y Opciones /////////*/
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -64,6 +183,13 @@ public class Materias extends AppCompatActivity {
             Informacion(null);
             return true;
         }
+
+        if (id == R.id.update){
+            Actualizar(null);
+            return true;
+        }
+
+
      return super.onOptionsItemSelected(opcion_menu);
 
     }
---
+++ app/src/main/java/ar/com/jsdrake/cali/PantallaActualizar.java
@@ -0,0 +1,62 @@
+package ar.com.jsdrake.cali;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import Clases.MyReceiver;
+
+public class PantallaActualizar extends AppCompatActivity {
+    MyReceiver oMyReceiver;
+    Button btn_descargar;
+
+    String url,version;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar);
+
+        version=getIntent().getStringExtra("version");
+        url=getIntent().getStringExtra("url");
+
+
+
+
+        Init();
+
+
+        btn_descargar=(Button) findViewById(R.id.btn_Actualizar);
+        btn_descargar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                oMyReceiver.Descargar(url);
+
+
+            }
+        });
+
+    }
+
+
+    private  void Init(){
+        oMyReceiver=new MyReceiver(PantallaActualizar.this);
+        oMyReceiver.registrar(oMyReceiver);
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        oMyReceiver.borrarRegistro(oMyReceiver);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        oMyReceiver.registrar(oMyReceiver);
+    }
+}
